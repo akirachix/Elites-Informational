1a642b46581faa39b0bad19cd541ee45c47750f0
gatchang-nyawargak <nyawargakgatchang@gmail.com>
2024-06-18 13:07:05 +0300

    added files

---
+++ umuhinzi-react/src/About/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+const About = () => (
+    <div className='about'>
+        <h2 id="heading">About Us</h2>
+        <p>
+            Umuhinzi platform empowers farmers by making them informed thus able to make better decisions when<br />
+            purchasing farm inputs Through this platform you will access the latest prices for input products.
+            <br />
+            You will also get news on what is happening in the tea sector.If you do not  have a smartphone,
+            <br />
+            no worries.We have USSD services for you where you will get the same features.
+        </p>
+    </div>
+);
+
+export default About;
