e6e84821168e3e32881f3b55d11d49e3ab56d4e9
Trecy-Malonza <trecymalonza@gmail.com>
2024-07-17 11:55:41 +0300

    added stylings

--- umuhinzi-react/src/Products/index.js
+++ umuhinzi-react/src/Products/index.js
@@ -8,44 +8,39 @@ function Products(){
        
         <div className='products'>
             <h1 className ='heading'>Products</h1>
+
+            <div className="webapp">
+
             <div className ="productbody">
+            <h5 className ='webtitle'>Umuhinzi Web App</h5>
             <img className = 'webimg'src={app} alt='app img'/>
             </div>
-              <div>
-          <h5 className ='webtitle'>Umuhinzi Web App</h5>
+
+            <div className = 'textweb'>
+            
+            <p className="info">Our products will show current market prices 
+          for different farming inputs,past purchases
+          and transaction history and update market pricing</p>
             </div>
-            <div className='webapp'>
-              <div>
-         
-          <div className='appinfo'>
-           <p>Our products will show current market prices 
-          for<br/> different farming inputs,past purchases
-          and transaction<br/> history and update market pricing</p>
-     </div>
-          </div>
+            </div>
+            
+           
+           
           <div className='ussd'>
           
           <div className='ussdinfo'>
           <div >
             <h3 className='app'>USSD</h3>
             </div>
-              <p>Umuhinzi is a USSD platform that helps
-              smallholder<br/> farmers have
-              access to input and information to <br/> boost their productivity
-            and to be market ready</p>
-            </div>
+              <p className='ussdtext'>Umuhinzi is a USSD platform that helps smallholder farmers have access to input and information to boost their productivity and to be market ready</p>
+
             
             <img src={ussd} alt='ussd' className='ussdimg'/>
             </div>
               
            </div>
             </div>
-        
-
-
-
-          
-       
     )
-}
-export default Products;
+  }
+      
+    export default Products
