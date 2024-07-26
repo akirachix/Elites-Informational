a321ecc2122126d69e5f662aebf4f8c798feebe6
Linet Ndau <mkandoelinet@gmail.com>
2024-07-26 11:25:05 +0300

    added changes

--- umuhinzi-react/src/App.js
+++ umuhinzi-react/src/App.js
@@ -4,17 +4,19 @@ import About from "./About";
 import Products from './Products';
 import Team from "./Team";
 import Footer from './Footer';
+import VisionAndMission from "./VisionAndMission";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-    <LandingPage/>
-       <About/>
+      <LandingPage/>
+      <About/>
+      <VisionAndMission/>
       <Products/>
       <Team/>
-    <Footer/>
+      <Footer/>
     </div>
 
 );
